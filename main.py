74ba2fe901a6acebe868fa5f06ed8c5ea23bfceb
Jeff Shuo <jeffshuo@gmail.com>
2026-02-26 18:26:42 +0800

    refactor: update ChatOpenAI model to gpt-4o-mini across multiple grader files and main.py

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     print("Hello Advanced RAG")
     # print(app.invoke(input={"question": "What is SELF-RAG?"}))
     # print(app.invoke(input={"question": "what is agent memory?"}))
-    print(app.invoke(input={"question": "who is jeff shuo? what country was he born in?"}))
+    print(app.invoke(input={"question": "who is ming hsiu cheng? what country was she born in?"}))
