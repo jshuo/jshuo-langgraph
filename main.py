7fccd03b8dbf0289912af25980a61996403659f6
Jeff Shuo <jeffshuo@gmail.com>
2026-02-26 16:54:00 +0800

    chore: add pyproject.toml for project dependencies and configuration

---
+++ main.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from graph.graph import app
+
+if __name__ == "__main__":
+    print("Hello Advanced RAG")
+    # print(app.invoke(input={"question": "What is SELF-RAG?"}))
+    # print(app.invoke(input={"question": "what is agent memory?"}))
+    print(app.invoke(input={"question": "who is jeff shuo? what country was he born in?"}))
